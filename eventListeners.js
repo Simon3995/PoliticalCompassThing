bfc0feb43840ebab62213ab2813ebbedcd2182b2
Frank Dragt <mailvoorfrank.dragt@gmail.com>
2020-11-27 14:33:33 +0100

    initial eventListener frameworks

---
+++ eventListeners.js
@@ -0,0 +1,19 @@
+let Events = {
+	drag: false
+};
+
+document.addEventListener('mousedown', e => {
+	Events.drag = true;
+	Events.initMousePos = [e.x, e.y];
+});
+
+document.addEventListener('mouseup', () => {
+	Events.drag = false;
+	Events.initMousePos = null;
+});
+
+document.addEventListener('mousemove' () => {
+	if (Events.drag) {
+		// TODO
+	}
+});

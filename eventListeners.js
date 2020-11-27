eef6aae4b3fb4ecaced2eb11cf56747988179982
Frank Dragt <mailvoorfrank.dragt@gmail.com>
2020-11-27 16:47:04 +0100

    made some boundaries the offset can't be moved by

--- eventListeners.js
+++ eventListeners.js
@@ -14,8 +14,15 @@ document.addEventListener('mouseup', () => {
 
 document.addEventListener('mousemove', e => {
 	if (Events.drag) {
+		// move canvas offset
 		canvas_stuff.offsetX += Events.prevMousePos[0] - e.x;
 		canvas_stuff.offsetY += Events.prevMousePos[1] - e.y;
+		// limit offset
+		canvas_stuff.offsetX = Math.min(canvas_stuff.offsetX, c.width / 2);
+		canvas_stuff.offsetX = Math.max(canvas_stuff.offsetX, -c.width / 2);
+		canvas_stuff.offsetY = Math.min(canvas_stuff.offsetY, c.height / 2);
+		canvas_stuff.offsetY = Math.max(canvas_stuff.offsetY, -c.height / 2);
+		
 		Events.prevMousePos = [e.x, e.y];
 	}
 });

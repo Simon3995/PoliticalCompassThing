cf8b433e08c919ddcc933894d1567666d93084f4
Simon <simonsimonsimon3995@gmail.com>
2020-11-29 13:46:42 +0100

    Changed blocky segments to ring segments

--- main.js
+++ main.js
@@ -19,14 +19,16 @@ function update() {
 	graphic.visualOffsetX = (2*graphic.visualOffsetX + graphic.offsetX) / 3;
 	graphic.visualOffsetY = (2*graphic.visualOffsetY + graphic.offsetY) / 3;
 	
-	ringCompass();
+	drawColorSegments();
+	drawSeparators();
+	drawCenterCircle();
 	drawText();
+	
 	window.requestAnimationFrame(update);
 }
 
-function ringCompass() {
+/*function drawColorSegments() {
 	let segmentsPerQuad = graphic.segmentsPerQuad;
-	
 	let center = [c.width / 2 - graphic.visualOffsetX, c.height / 2 - graphic.visualOffsetY];
 	
 	ctx.lineWidth = 150;
@@ -40,7 +42,7 @@ function ringCompass() {
 		nextColor = colors[index-1 & 3];
 		for (let i = 0; i < segmentsPerQuad; i++) {
 			let segmentCenter = i / (segmentsPerQuad + 1);
-			segmentCenter = (1 + Math.tanh(10 * (segmentCenter - 0.5))) / 2;
+			segmentCenter = (1 + Math.tanh(10000 * (segmentCenter - 0.5))) / 2;
 			let interColor = [];
 			for (let freq in color) {
 				interColor[freq] = segmentCenter * color[freq] + (1 - segmentCenter) * nextColor[freq];
@@ -59,6 +61,32 @@ function ringCompass() {
 			ctx.stroke();
 		}
 	});
+}*/
+
+function drawColorSegments() {
+	let center = [c.width / 2 - graphic.visualOffsetX, c.height / 2 - graphic.visualOffsetY];
+	
+	ctx.lineWidth = 55;
+	let hs = [220, 10, 130, 50];
+	
+	for (let j = 0; j < 3; j++) {
+		for (let i = 0; i < 4; i++) {
+			ctx.strokeStyle = "hsl(" + hs[i] + ", 65%, " + (65-7*j) + "%)";
+			ctx.beginPath();
+			ctx.arc(
+				...center,
+				200 + 50*j,
+				-i * Math.PI / 2,
+				-(i+1.01) * Math.PI / 2,
+				true
+			);
+			ctx.stroke();
+		}
+	}
+}
+
+function drawSeparators() {
+	let center = [c.width / 2 - graphic.visualOffsetX, c.height / 2 - graphic.visualOffsetY];
 	
 	// draw the dividing lines
 	ctx.strokeStyle = "#222";
@@ -72,6 +100,10 @@ function ringCompass() {
 		ctx.lineTo(...dat[1]);
 		ctx.stroke();
 	});
+}
+
+function drawCenterCircle() {
+	let center = [c.width / 2 - graphic.visualOffsetX, c.height / 2 - graphic.visualOffsetY];
 	
 	// draw a center circle
 	ctx.fillStyle = "#fff5";
